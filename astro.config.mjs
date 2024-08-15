0e718d90bf18e3e84b3decb943c15752efd76b5f
James Lloyd <james.lloyd@gmail.com>
2024-08-15 17:54:36 +0100

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://james.lloyd.ws',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://james.lloyd.ws',
+})
